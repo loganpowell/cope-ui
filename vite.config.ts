3e888fc20b6f924a302c881c980c4344d98e800f
Logan Powell <loganp@tepper.cmu.edu>
2021-12-02 15:13:59 -0500

    enables emotion and fixes imports

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [react()],
+  // https://dev.to/ajitsinghkamal/using-emotionjs-with-vite-2ndj
+  esbuild: {
+    jsxFactory: `jsx`,
+    jsxInject: `import { jsx } from '@emotion/react'`,
+  },
+});
